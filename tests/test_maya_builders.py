b336a5abbddebe7f3a1e2071a673c68cbc1ee2e6
howard <bhowiebkr@gmail.com>
2019-09-09 23:44:33 -0400

    unittest framework init

---
+++ tests/test_maya_builders.py
@@ -0,0 +1,11 @@
+import unittest
+
+import jbeamMayaTools.core.maya_builders as maya_builders
+
+
+class TestMayaBuilders(unittest.TestCase):
+    def test_make_beam(self):
+        pass
+
+    def test_make_node(self):
+        pass
